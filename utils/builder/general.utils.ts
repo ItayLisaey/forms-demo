83c26d6dbebc91e96fa4466d32cfea4e772775f4
Itay Lisaey <et030102@gmail.com>
2021-12-31 00:13:54 +0200

    clean(utils): removed unused stackoverflow utils

--- utils/builder/general.utils.ts
+++ utils/builder/general.utils.ts
@@ -12,38 +12,6 @@ export const optionsCheck = (type: FormTypes | undefined) => {
   }
 };
 
-// "arrayMove" sauce: https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another?rq=1
-
-export function arrayMove<T>(arr: T[], fromIndex: number, toIndex: number) {
-  const element = arr[fromIndex];
-  arr.splice(fromIndex, 1);
-  arr.splice(toIndex, 0, element);
-}
-
-// "changeIndex" sauce: https://www.akashmittal.com/change-position-of-element-in-array/
-
-export const changeIndex = (
-  arrayToUpdate: FormFieldTemplate[],
-  elementToReposition: FormFieldTemplate,
-  newIndex: number
-) => {
-  if (newIndex > arrayToUpdate.length - 1) {
-    console.log('Array index out of bounds');
-    return null;
-  } else if (!arrayToUpdate.some((el) => el.id === elementToReposition.id)) {
-    console.log('Element not found');
-    return null;
-  }
-
-  const elementIndex = arrayToUpdate.findIndex(
-    (el) => el.id === elementToReposition.id
-  );
-
-  arrayToUpdate.splice(elementIndex, 1);
-  arrayToUpdate.splice(elementIndex, newIndex);
-  return arrayToUpdate;
-};
-
 export const moveItem = <T>(
   index: number,
   arr: T[],
